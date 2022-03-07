83894398fcee6bc0b94ee5751efd3a664fae71c7
Sreyas S <76519139+sreyasqb@users.noreply.github.com>
2022-03-07 16:21:44 +0530

    updated

--- employee.sql
+++ employee.sql
@@ -1,4 +1,4 @@
-create table employee (
+create table employeeagi (
   empid varchar(16),
   emp_name varchar(20),
   DOB date,
@@ -11,33 +11,35 @@ create table employee (
   dept number(10)
 );
 --1a
-alter table employee add primary key(empid);
-alter table employee add constraint empid check(regexp_like(empid,'PSG[0-9]{3}'));
+alter table employeeagi add primary key(empid);
+alter table employeeagi add constraint empid2 check(regexp_like(empid,'PSG[0-9]{3}'));
 
 --1b
-alter table employee add unique(mobile_no);
-alter table employee add constraint mno check(length(to_char(mobile_no))=10);
+alter table employeeagi add unique(mobile_no);
+alter table employeeagi add constraint mno2 check(length(to_char(mobile_no))=10);
 -- testing if it works
-insert into employee(empid,mobile_no) values ('PSG900',1134567890);
-insert into employee(empid,mobile_no) values ('PSG800',12234567890);
+insert into employeeagi(empid,mobile_no) values ('PSG900',1134567890);
+insert into employeeagi(empid,mobile_no) values ('PSG800',12234567890);
 
 --1c
-alter table employee add constraint sal check(salary between 60000 and 20000);
+alter table employeeagi add constraint sal3 check(salary between 60000 and 10000);
+alter table employeeagi drop constraint sal2;
 
 --1d
---alter table employee add constraint gen check(regexp_like(gender,'[M|F'))
-alter table employee add constraint gen check(gender='M' or gender='F');
+--alter table employeeagi add constraint gen check(regexp_like(gender,'[M|F'))
+alter table employeeagi add constraint gen check(gender='M' or gender='F');
 
 --1e
-alter table employee add constraint age_check check(months_between(dob,doj)>300);
+alter table employeeagi add constraint age_check2 check(months_between(dob,doj)>300);
+alter table employeeagi drop constraint age_check;
 
 --1f
-alter table employee modify(emp_name not null);
-alter table employee modify(project_domain not null);
-alter table employee add constraint emp_proj check(emp_name not LIKE '%[0-9]%' and project_domain not LIKE '%[0-9]%');
+alter table employeeagi modify(emp_name not null);
+alter table employeeagi modify(project_domain not null);
+alter table employeeagi add constraint emp_proj check(emp_name not LIKE '%[0-9]%' and project_domain not LIKE '%[0-9]%');
 
 --1g
-alter table employee add foreign key(dept) references department(dept_id);
+alter table employeeagi add foreign key(dept) references department(dept_id);
 
 --2a
 create table department(
@@ -47,5 +49,39 @@ create table department(
   
 );
 
---alter table employee add constraint nodata check()
 
+
+--alter table employeeagi add constraint nodata check()
+--3
+--1
+select empid,emp_name,salary from employeeagi where (project_domain='Banking');
+--2
+select count(empid) as programmers from employeeagi where (designation='Programmer');
+--3
+select designation,count(designation) from employeeagi group by designation order by count(designation);
+--4
+select dept_name,count(dept_name) from departmentagi group by dept_name;
+select dept_name from departmentagi , empid from employeeagi;
+select empid, dept_id from departmentagi , employeeagi;
+--5
+select count(*) as houstonkids from employeeagi e inner join departmentagi d on d.dept_id=e.dept where d.dept_loc='Houston';
+--6
+select empid,emp_name,0.05*salary as increased_sal from employeeagi order by increased_sal desc; 
+--7
+select count(empid) as mobileappkids from employeeagi where project_domain='MobileApp';
+--8
+
+--copy template
+--select count(*) from employeeagi e inner join departmentagi d on d.dept_id=e.dept where 
+
+select * from employeeagi e left join departmentagi d on d.dept_id=e.dept where d.dept_loc='Sugarland';
+
+--9
+
+select emp_name from employeeagi where salary=(select max(salary) as sal_max from employeeagi e inner join ( select project_domain,count(*) as members from employeeagi group by project_domain having count(*)>2) t on t.project_domain=e.project_domain);
+
+--10
+
+select emp_name from employeeagi where extract(month from doj)=(select month_no from (select count(extract(month from doj)) as kids,extract(month from doj) as month_no from employeeagi where designation='Programmer' group by extract(month from doj) order by kids desc) where rownum<2);
+
+--11
