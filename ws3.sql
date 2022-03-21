08a76db4dc8376d41d1fee52391f2cda52e4264c
Sreyas S <76519139+sreyasqb@users.noreply.github.com>
2022-03-21 15:18:03 +0530

    finished ws3

--- ws3.sql
+++ ws3.sql
@@ -62,4 +62,76 @@ insert into shipment values (4,3,null,null);
 insert into shipment values (4,4,300,0.006);
 insert into shipment values (4,5,400,0.003);
 
+--questions
+
+--1
+select * from shipment ship inner join part p on ship.pno=p.pno where sno=2;
+--2
+select p.pname,p2.pname,p.color from part p2 inner join part p on p2.color=p.color where p.pno!=p2.pno;
+--3
+select s1.sname,s2.sname,s1.city from supplier s1 inner join supplier s2 on s2.city=s1.city where  s1.sno!=s2.sno;
+--4
+select * from supplier where city!='Paris';
+--5
+select sno as sales_guy from shipment group by sno having count(pno)= (select count(*) from part);
+--6
+select * from shipment where qty between 300 and 750;
+--7
+select distinct p.pname from shipment ship inner join part p on ship.pno=p.pno where ship.qty between 200 and 750;
+--8
+select unique(sup.sname) from shipment ship inner join supplier sup on ship.sno=sup.sno where ship.pno=2 or ship.pno=4 ;
+--9
+select pno from shipment where sno=3;
+--select * from shipment where sno = all(select pno from shipment where sno=3);
+--10
+select distinct(sno) from shipment ship inner join part p on ship.pno=p.pno where color='Red';
+--11
+select distinct pname from (select count(*) as count_no,pno as part_no from shipment ship group by pno having(count(*))>2) p INNER JOIN part s ON s.pno = p.part_no;
+--select count(*) as count_no,pno as part_no from shipment ship group by pno having(count(*))>2;
+--12
+select sno,sname from supplier where sno not in (select ship.sno from shipment ship inner join part p on p.pno=ship.pno where color='Red');
+--13
+--select * from shipment ship inner join part p on ship.pno=p.pno ;
+--select * from supplier ;
+--14
+select * from supplier where sno not in (select distinct sno from shipment);
+--15
+select distinct(sno) from shipment where pno in (select pno from shipment where sno=2) and sno!=2;
+--16
+select p.pname from shipment sh inner join part p on sh.pno=p.pno where sno=(select sno from supplier where sname='Blake') ;
+--17
+select * from shipment where not price is null order by price desc;
+--18
+select * from supplier where city in (select city from supplier where sno=1) and sno!=1;
+--19
+
+select pno from shipment group by pno having count(pno)>1;
+--20
+select sno from supplier where status<(select avg(status) from supplier);
+--21
+select count(sno) as count_no from supplier;
+--22
+select count(unique(sno)) as count_no from shipment;
+
+--23
+select s.sname,p.pname from shipment sh inner join part p on sh.pno=p.pno inner join supplier s on s.sno=sh.sno where price/qty =(select max(price/qty) from shipment); 
+--24
+select s.sname,sa.max_price,s.sno from (select max(price) as max_price,sno from shipment group by sno order by max_price desc) sa inner join supplier s on s.sno=sa.sno where rownum<3;
+--25
+select s.*,sa.total from (select sno,sum(price*qty) as total from shipment group by sno order by total desc) sa inner join supplier s on sa.sno=s.sno where rownum<2;
+--26
+select sno,count(pno) as cpno from shipment group by sno;
+--27
+select s.sname,s.sno,s.status from (select sno,sum(qty) as sum_qty from shipment group by sno having sum(qty)=1300) sa inner join supplier s on sa.sno=s.sno;
+--28
+--doesnt make sense question
+--29
+
+delete from part where pno=(select pno from (select pno,count(*) as number_parts from shipment group by pno order by number_parts) where rownum<2);
+--30
+update supplier set status=50 where sno=(select sno from (select sno,sum(qty*price) as total from shipment group by sno order by total desc) where rownum<2);
+--31
+CREATE VIEW viewpart AS SELECT p.* FROM part p inner join shipment sh on p.pno=sh.pno;
+
+
 
